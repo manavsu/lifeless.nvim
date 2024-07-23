21de46d495ff726c7b6f608e2cb5ba63a2f0e1ae
csgn <dev.csgn@gmail.com>
2024-07-23 10:07:03 +0300

    f

--- lua/nolife/colors.lua
+++ lua/nolife/colors.lua
@@ -1,81 +1,80 @@
 local config = require("nolife.config")
 
 local colors = {
-	white = "#ffffff",
-	black = "#000000",
+    white = "#ffffff",
+    black = "#000000",
 }
 
 if vim.o.background == "light" then
-	-- background colors
-	colors.bg = config.transparent and "NONE" or "#ffffff"
-	colors.bgDark = "#dddddd"
-	colors.bgDarker = "#f6f6f6"
-	colors.bgFloat = "#e0e0e0"
-	colors.bgOption = "#ededed"
-	-- foreground colors
-	colors.fg = "#616161"
-	colors.fgAlt = "#212121"
-	colors.fgCommand = "#333333"
-	colors.fgInactive = "#9e9e9e"
-	colors.fgDisabled = "#d0d0d0"
-	colors.fgLineNr = "#a1a1a1"
-	colors.fgSelection = "#424242"
-	colors.fgSelectionInactive = "#757575"
-	-- border colors
-	colors.border = "#c2c3c5"
-	colors.borderFocus = "#aaaaaa"
-	colors.borderDarker = "#999999"
-	-- ui colors
-	colors.blue = "#bdbdbd" -- light gray
-	colors.red = "#9e9e9e" -- gray
-	colors.purple = "#7e7e7e" -- gray
-	colors.redDark = "#616161" -- dark gray
-	colors.orange = "#8d8d8d" -- gray
-	colors.blueLight = "#d3d3d3" -- lighter gray
-	colors.comment = "#a8a8a8" -- gray
-	colors.orangeLight = "#cacaca" -- light gray
-	colors.green = "#b1b1b1" -- gray
-	colors.yellowDark = "#d4d4d4" -- lighter gray
-	colors.purpleDark = "#565656" -- dark gray
-	colors.symbol = "#b5b5b5" -- gray
-	colors.primary = "#000000" -- black
-	colors.terminalerightblack = "#4a4a4a" -- dark gray
+    -- background colors
+    colors.bg = config.transparent and "NONE" or "#ffffff"
+    colors.bgDark = "#e0e0e0"
+    colors.bgDarker = "#d0d0d0"
+    colors.bgFloat = "#f0f0f0"
+    colors.bgOption = "#ededed"
+    -- foreground colors
+    colors.fg = "#000000"
+    colors.fgAlt = "#333333"
+    colors.fgCommand = "#444444"
+    colors.fgInactive = "#666666"
+    colors.fgDisabled = "#999999"
+    colors.fgLineNr = "#555555"
+    colors.fgSelection = "#000000"
+    colors.fgSelectionInactive = "#888888"
+    -- border colors
+    colors.border = "#bbbbbb"
+    colors.borderFocus = "#888888"
+    colors.borderDarker = "#555555"
+    -- ui colors
+    colors.blue = "#0000ff"
+    colors.red = "#ff0000"
+    colors.purple = "#800080"
+    colors.redDark = "#8b0000"
+    colors.orange = "#ff8c00"
+    colors.blueLight = "#add8e6"
+    colors.comment = "#2e8b57"
+    colors.orangeLight = "#ffd700"
+    colors.green = "#00ff00"
+    colors.yellowDark = "#ffdf00"
+    colors.purpleDark = "#4b0082"
+    colors.symbol = "#4682b4"
+    colors.primary = "#000000"
+    colors.terminalerightblack = "#2f4f4f"
 else
-	-- background colors
-	colors.bg = config.transparent and "NONE" or "#212121"
-	colors.bgDark = "#1a1a1a"
-	colors.bgDarker = "#292929"
-	colors.bgFloat = "#383838"
-	colors.bgOption = "#282828"
-	-- foreground colors
-	colors.fg = "#c1c1c1"
-	colors.fgAlt = "#fafafa"
-	colors.fgCommand = "#e0e0e0"
-	colors.fgInactive = "#484848"
-	colors.fgDisabled = "#848484"
-	colors.fgLineNr = "#727272"
-	colors.fgSelection = "#eaeaea"
-	colors.fgSelectionInactive = "#f5f5f5"
-	-- border colors
-	colors.border = "#2a2a2a"
-	colors.borderFocus = "#444444"
-	colors.borderDarker = "#363636"
-	-- ui colors
-	colors.red = "#8d8d8d" -- gray
-	colors.blue = "#a9a9a9" -- light gray
-	colors.orange = "#bfbfbf" -- light gray
-	colors.purple = "#c7c7c7" -- light gray
-	colors.blueLight = "#d3d3d3" -- lighter gray
-	colors.orangeLight = "#dfdfdf" -- light gray
-	colors.yellowDark = "#cdcdcd" -- light gray
-	colors.green = "#b1b1b1" -- gray
-	colors.redDark = "#616161" -- dark gray
-	colors.purpleDark = "#565656" -- dark gray
-	colors.comment = "#a8a8a8" -- gray
-	colors.symbol = "#bbbbbb" -- gray
-	colors.primary = "#ffffff" -- white
-	colors.terminalBrightBlack = "#5c5c5c" -- dark gray
+    -- background colors
+    colors.bg = config.transparent and "NONE" or "#000000"
+    colors.bgDark = "#1a1a1a"
+    colors.bgDarker = "#0d0d0d"
+    colors.bgFloat = "#2e2e2e"
+    colors.bgOption = "#333333"
+    -- foreground colors
+    colors.fg = "#ffffff"
+    colors.fgAlt = "#eeeeee"
+    colors.fgCommand = "#dddddd"
+    colors.fgInactive = "#aaaaaa"
+    colors.fgDisabled = "#888888"
+    colors.fgLineNr = "#ffffff"
+    colors.fgSelection = "#ffffff"
+    colors.fgSelectionInactive = "#cccccc"
+    -- border colors
+    colors.border = "#444444"
+    colors.borderFocus = "#888888"
+    colors.borderDarker = "#222222"
+    -- ui colors
+    colors.blue = "#87cefa"
+    colors.red = "#ff4500"
+    colors.orange = "#ffa500"
+    colors.purple = "#da70d6"
+    colors.blueLight = "#b0c4de"
+    colors.orangeLight = "#ffdab9"
+    colors.yellowDark = "#ffd700"
+    colors.green = "#32cd32"
+    colors.redDark = "#8b0000"
+    colors.purpleDark = "#800080"
+    colors.comment = "#8a2be2"
+    colors.symbol = "#87ceeb"
+    colors.primary = "#ffffff"
+    colors.terminalBrightBlack = "#696969"
 end
 
 return colors
-
